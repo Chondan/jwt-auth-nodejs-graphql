f462c1b0f72de0d2c0c8d2e2f41cb4cf438dc50c
Chondan <chondansusuwan@outlook.com>
2021-07-02 20:19:19 +0100

    Initialise server project

---
+++ README.md
@@ -0,0 +1,27 @@
+# JWT Authentication Node.js Tutorial with GraphQL and React
+
+> Link: https://www.youtube.com/watch?v=25GS0MLT8JU
+
+## Technologies
+
+- [x] TypeScript
+- [x] GraphQL
+- [ ] TypeGraphQL
+- [ ] [TypeORM](https://typeorm.io/#/)
+- [x] PostgresQL
+- [x] React
+- [ ] Apollo
+
+## Lesson learned
+
+- `yarn upgrade-interactive --latest` to upgrade the dependencies
+- PostgreSQL
+    - quick commands:
+        - login: `psql postgres`
+        - list databases: `\l`
+        - switching databases: `\c database-name`
+        - list tables: `\dt`
+        - create database: `createdb database-name`
+    - [Getting Started with PostgreSQL on Mac OSX](https://www.codementor.io/@engineerapart/getting-started-with-postgresql-on-mac-osx-are8jcopb)
+- [5 minutes vim: enabling vim in zsh](https://www.barbarianmeetscoding.com/blog/5-minutes-vim-enabling-vim-in-zsh)
+- [fish terminal](https://fishshell.com/)
---
+++ server/src/index.ts
@@ -0,0 +1,31 @@
+import "reflect-metadata";
+// import {createConnection} from "typeorm";
+import express from 'express';
+// import {User} from "./entity/User";
+
+
+(async () => {
+
+    const app = express();
+    app.get('/', (_req, res) => res.send('Hello World'));
+    app.listen(3000, () => console.log('Server started: Listening at port 3000'));
+    
+})();
+
+// createConnection().then(async (connection: { manager: { save: (arg0: User) => any; find: (arg0: typeof User) => any; }; }) => {
+
+//     console.log("Inserting a new user into the database...");
+//     const user = new User();
+//     user.firstName = "Timber";
+//     user.lastName = "Saw";
+//     user.age = 25;
+//     await connection.manager.save(user);
+//     console.log("Saved a new user with id: " + user.id);
+
+//     console.log("Loading users from the database...");
+//     const users = await connection.manager.find(User);
+//     console.log("Loaded users: ", users);
+
+//     console.log("Here you can setup and run express/koa/any other framework.");
+
+// }).catch((error: any) => console.log(error));
