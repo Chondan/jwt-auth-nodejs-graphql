c9be51b96b215ed1994358718cc35f163fc5bee3
Chondan <chondansusuwan@outlook.com>
2021-07-03 15:28:21 +0700

    Setup project
    * setup eslint
    * setup alias path using tsconfig-paths
    * add env file
    * add usefule dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,31 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        mjs: 'never',
+      },
+    ],
+    'no-unused-vars': 'warn',
+    'import/no-unresolved': 'off',
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      },
+    },
   },
 };
