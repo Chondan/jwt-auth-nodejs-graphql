0478c309d3e9570e44a0246d68235c0bee54ec3b
Chondan <chondansusuwan@outlook.com>
2021-07-10 05:26:20 +0700

    Add 'tokenRefreshLink' to apollo client to seamlessly fetch token in the background

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,21 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import { RouteComponent } from '@src/Routes';
+import {
+  ApolloClient, ApolloProvider, createHttpLink, InMemoryCache, ApolloLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { onError } from '@apollo/client/link/error';
+import { TokenRefreshLink } from 'apollo-link-token-refresh';
+import { AccessToken, getRefreshTokenEndpoint } from '@src/util/token';
 import reportWebVitals from './reportWebVitals';
 import './index.scss';
+import { App } from './App';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+  credentials: 'include',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = AccessToken.get();
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${AccessToken.get()}` : '',
+    },
+  };
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  // eslint-disable-next-line no-console
+  console.error({ graphQLErrors, networkError });
+});
+
+const tokenRefreshLink = new TokenRefreshLink({
+  accessTokenField: 'accessToken',
+  isTokenValidOrUndefined: () => {
+    const token = AccessToken.get();
+
+    if (!token) return true;
+
+    try {
+      return !AccessToken.isExpired(token);
+    } catch (err) {
+      return false;
+    }
+  },
+  fetchAccessToken: () => fetch(getRefreshTokenEndpoint(), {
+    method: 'POST',
+    credentials: 'include',
+  }),
+  handleFetch: (accessToken) => {
+    // eslint-disable-next-line no-console
+    console.log('fetched new access token');
+    AccessToken.set(accessToken);
+  },
+  handleError: (err) => {
+    // eslint-disable-next-line no-console
+    console.warn('Your refresh token is invalid. Try to relogin');
+    // eslint-disable-next-line no-console
+    // console.error(err);
+
+    // your custom action here e.g. user.logout()
+  },
+});
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3001/graphql',
   cache: new InMemoryCache(),
   credentials: 'include',
-
+  link: ApolloLink.from([
+    tokenRefreshLink as any,
+    errorLink,
+    authLink,
+    httpLink,
+  ]),
 });
 
 ReactDOM.render(
     <React.StrictMode>
         <ApolloProvider client={client}>
-            <RouteComponent />
+            <App />
         </ApolloProvider>
     </React.StrictMode>,
     document.getElementById('root'),
